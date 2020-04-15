c35c6bbc30bfa53274134e17bfbdc3989ad88539
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-04-15 21:29:52 +0200

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -14,7 +14,7 @@ import os
 import wget
 
 if os.getenv("model")!=None:
-    wget.download(os.getenv("model"), os.getcwd())
+    wget.download(os.getenv("model"), os.getcwd()+"/model.bin")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
