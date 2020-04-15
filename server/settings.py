cb704cdc084a93dc4297fb2262df446818bc2dd0
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-04-15 21:39:49 +0200

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -13,12 +13,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import wget
 
-if os.getenv("model")!=None:
-    wget.download(os.getenv("model"), os.getcwd()+"/model.bin")
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if os.getenv("model")!=None:
+    wget.download(os.getenv("model"), os.path.join(BASE_DIR+"/model.bin"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
