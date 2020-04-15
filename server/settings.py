dd208b1f11f5c69c1ee552952ab469e5d33f8f13
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-04-15 21:26:53 +0200

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-#import django_heroku
-
 import wget
-wget.download(os.getenv("model"), os.getcwd())
+
+if os.getenv("model")==None:
+    print("Model must be specified in env variable")
+else :
+    wget.download(os.getenv("model"), os.getcwd())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
